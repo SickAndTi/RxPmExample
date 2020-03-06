e7db05c6c04148d261246da9114cbd4f70ca9f05
sickandtired <sickandtired88888@gmail.com>
2020-03-06 23:20:50 +0300

    - app class  - add other dependencies

---
+++ app/src/main/java/com/krit/App.kt
@@ -0,0 +1,16 @@
+package com.krit
+
+import android.app.Application
+import timber.log.Timber
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+
+    }
+
+
+}
